111605c828e83f81a71ec7c34f372263bd49cccd
Mansha Singhal <manshasinghal04@gmail.com>
2025-08-11 20:11:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose'); // You need this to connect to MongoDB
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/tasks';
 
-
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB successfully on Vercel!');
+}).catch(err => {
+  console.error('Could not connect to MongoDB:', err);
+});
 
 const userRouter = require('./router/Userroute');
 app.use('/api', userRouter);
 
 const taskRouter = require('./router/taskRouter');
-app.use('/api',taskRouter )
-
-
-
-
-
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.use('/api', taskRouter);
 
 module.exports = app;
